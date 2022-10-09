bcd33b30fdb8a683eca1987afe89538b781e0f1c
arifa <arifa.mansoor@mail.utoronto.ca>
2022-10-08 20:53:56 -0400

    Intro to Java Exercise 1

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -39,10 +39,7 @@ public class Basics {
          *    (Relevant reading: 1.1.3. Printing things)
          */
         System.out.println(7 + 5);
-
-        /* TODO (Task 1): Write a line of code below that prints the string
-         *                Hello World!
-         */
+        System.out.println("Hello World!");
 
 
 
@@ -58,10 +55,7 @@ public class Basics {
          *    (Relevant reading: 1.2. Variables and Types)
          */
 
-        /*
-         * TODO (Task 2): Create a variable named my_variable and assign it the
-         *                value 100.
-         */
+        int my_variable = 100;
 
 
 
@@ -99,7 +93,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
-
+        for(int j = 10; j > -1; j--){
+            System.out.println("Current count: " + j);
+        }
 
     }
 
@@ -141,8 +137,9 @@ public class Basics {
          *                you may use them if you wish.
          */
         StringBuilder ret = new StringBuilder();
-
-        // Fill in the rest of the body here
+        String[] split = to_split.split(" ", 8);
+        for (String a : split)
+            System.out.print(a.charAt(0));
 
         return ret.toString();
     }
@@ -162,16 +159,21 @@ public class Basics {
      * @return       The sum of all integers at odd indices in arr
      */
     public static int oddSum(int[] arr) {
-        int current_sum = 0;
-
         /* TODO (Task 5): Complete this method body using a for-loop.
          *                You can find the length of an array by using the
          *                .length attribute (e.g. arr.length)
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
+        int current_sum = 0;
 
+        for(int i = 0; i < arr.length; i ++){
+            if(i % 2 != 0){
+                current_sum += arr[i];
+            }
+        }
         return current_sum;
+
     }
 
 
